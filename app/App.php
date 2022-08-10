d1113bb9489ad3d556982da2d5c897bf1874ff5b
tobiDosumu <dosunmumishael@gmail.com>
2022-08-10 23:18:33 +0100

    update app/App.php to include file path\n\ - add function to convert file data into array\n\ - add function to color expense red and income green.

--- app/App.php
+++ app/App.php
@@ -3,3 +3,38 @@
 declare(strict_types = 1);
 
 // Your Code
+$file_path = '../transaction_files/sample_1.csv';
+
+// convert file to array
+function file_to_array($file_path) {
+    $file_handle = fopen($file_path, 'r');
+    while (!feof($file_handle) ) {
+        $line_of_text[] = fgetcsv($file_handle);
+    }
+    fclose($file_handle);
+    return $line_of_text;
+}
+
+// call and pass file path into file_to_array()
+$transactions_array = file_to_array($file_path);
+
+// remove the first elements in the array
+array_shift($transactions_array);
+
+// color expenses
+function changeExpenseColor($amount) {
+
+    if ($amount < 0) {
+        echo '<td style="color:red">' . $amount . '</td>';
+    } else {
+        echo '<td style="color:green">' . $amount . '</td>';
+    }
+
+}
+
+
+
+
+// echo '<pre>';
+// print_r($transactions_array);
+// echo '</pre>';
